face880de6ac1d8eb1ba339a1831b729dbf6ca94
SantiagoC16 <6317@holbertonstudents.com>
2023-05-18 08:30:21 -0700

    prototypes

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - a function that sorts a doubly linked list of integers in
+ * ascending order using the Insertion sort algorithm
+ * @list: idk yet
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - a function that sorts an array of integers in
+ * ascending order using the Selection sort algorithm
+ * @array: the array of ints
+ * @size: the size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+
+}
