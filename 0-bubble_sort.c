838693b99f070fec90afd6412e0b8e7ecf821587
SantiagoC16 <6317@holbertonstudents.com>
2023-05-18 12:27:45 -0700

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,5 +10,10 @@
 
 void bubble_sort(int *array, size_t size)
 {
+    size_t count1 = 0, count2 = 0;
 
+    for (count1 = size - 1; count1 >= 0; count1--)
+        for (count2 = 1; count2 <= count1; count2++)
+            if (array[count2] > array[count2 + 1])
+                swap (array[count2], array[count2 + 1]);
 }
