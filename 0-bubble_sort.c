bc1d5edea28fdc91fa8c4f122ebb9208662e7e5f
SantiagoC16 <6317@holbertonstudents.com>
2023-05-18 12:56:51 -0700

    task 0 and fix to .h

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,6 +2,21 @@
 #include "sort.h"
 
 /**
+ * swap - swap 2 numbers
+ * @i: int 1
+ * @j: int 2
+ */
+
+void swap(int *i, int *j)
+{
+	int temp;
+	
+	temp = *i;
+	*i = *j;
+	*j = temp;
+}
+
+/**
  * bubble_sort - a function that sorts an array of integers in
  * ascending order using the Bubble sort algorithm
  * @array: the array of ints
@@ -10,23 +25,13 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t count1 = 0, count2 = 0;
+	size_t count1 = 0, count2 = 0;
 
-    for (count1 = size - 1; count1 >= 0; count1--)
-        for (count2 = 1; count2 <= count1; count2++)
-            if (array[count2] > array[count2 + 1])
-                swap (array[count2], array[count2 + 1]);
-}
-
-/**
- * swap - swap 2 numbers
- * 
- * 
-*/
-void swap (int i, int j)
-{
-    int temp;
-    temp = i;
-    i = j;
-    j = temp;
+	for (count1 = size - 1; count1 != 0; count1--)
+	{
+		puts("caca  ");
+		for (count2 = 1; count2 < count1; count2++)
+			if (array[count2] > array[count2 + 1])
+				swap(&array[count2], &array[count2 + 1]);
+	}
 }
--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#define SORT_H
 #ifndef SORT_H
+#define SORT_H
 
 #include <stddef.h>
 #include <stdio.h>
@@ -25,6 +25,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
+void swap (int * i, int * j);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
