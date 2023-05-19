474582c36b712939033e98d68d092de909c4fca5
SantiagoC16 <6317@holbertonstudents.com>
2023-05-19 09:14:32 -0700

    task 0 null case fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,6 +27,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t count1 = 0, count2 = 0;
 
+	if (array == NULL || size < 2)
+		return;
+	
 	for (count1 = size - 1; count1 != 0; count1--)
 	{
 		for (count2 = 0; count2 < count1; count2++)
