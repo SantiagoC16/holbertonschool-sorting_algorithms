dea75812cba6c9f8658d3abc0f02d65409a4618e
gabrielblasina <5722@holbertonstudents.com>
2023-05-23 21:02:59 -0700

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,8 +7,21 @@
  * @array: the array of ints
  * @size: the size of the array
  */
-
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min_idx, temp;
 
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		temp = array[min_idx];
+		array[min_idx] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+	}
 }
