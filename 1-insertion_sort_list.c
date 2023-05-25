25326b34ca2abb5e29192dcb493cb5545fd6bbc7
SantiagoC16 <6317@holbertonstudents.com>
2023-05-24 17:43:34 -0700

    task 1 added print list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -22,6 +22,7 @@ void insertion_sort_list(listint_t **list)
 			current->next = sorted;
 			if (sorted != NULL)
 				sorted->prev = current;
+			print_list(*list);
 			sorted = current;
 		}
 		else
@@ -35,6 +36,7 @@ void insertion_sort_list(listint_t **list)
 			current->next = temp->next;
 			if (temp->next != NULL)
 				temp->next->prev = current;
+			print_list(*list);
 			temp->next = current;
 			current->prev = temp;
 		}
