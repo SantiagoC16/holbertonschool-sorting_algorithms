a390b1c44293f254daffedd97fd0c71564ee9682
SantiagoC16 <6317@holbertonstudents.com>
2023-05-27 13:01:47 -0700

    task11

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,8 @@ void insertion_sort_list(listint_t **list)
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *current = (*list)->next, *temp, *pretemp;
+	listint_t *current = (*list)->next;
+	listint_t *temp, *pretemp;
 
 	while (current != NULL)
 	{
